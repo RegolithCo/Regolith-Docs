59b953474f036ec6ca071362e724972bf6d3c53e
Raychaser <198485567+RaychaserRegolith@users.noreply.github.com>
2025-07-18 08:44:01 -0700

    fixing broken site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,11 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/General">
+          <Link className="button button--secondary button--lg" to="/general">
             {/* <img src="/img/RockIcon.svg" alt="Survey Corps Logo" className={styles.logo} /> */}
             General Documentation
           </Link>
-          <Link className="button button--secondary button--lg" to="/intro">
+          <Link className="button button--secondary button--lg" to="/survey-corps">
             {/* <img src="/img/SurveyorLogo.svg" alt="Survey Corps Logo" className={styles.logo} /> */}
             Survey Corps
           </Link>
